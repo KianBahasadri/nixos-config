dfc1f6808175ec39f0e636e9081c67c657c14c91
Kian_Bahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-04-13 16:47:26 -0400

    getting rizzy in here

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# nixos-config
+# home-manager-config
 
+my configuration.nix is managed by https://github.com/tech-bros-2/nixos
 please tell me how to make better, thanks 🙏🙏🙏
 (no need to be polite)
--- home-manager.nix
+++ home-manager.nix
@@ -1,27 +1,18 @@
 { config, pkgs, ... }:
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
-in
+
 {
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
+  home.username = "kian";
+  home.stateVersion = "24.11";
 
-  home-manager.users.kian = {
-    home.stateVersion = "24.11";
-    home.packages = with pkgs; [
-    ];
+  programs.home-manager.enable = true;
 
-    programs.home-manager.enable = true;
-
-    programs.bash.enable = true;
-    programs.bash.shellAliases = {
-      l="ls";
-      ll="ls -l";
-    };
-
-    programs.git.enable = true;
-    programs.git.userName = "KianBahasadri";
-    programs.git.userEmail = "101868619+KianBahasadri@users.noreply.github.com";
+  programs.bash.enable = true;
+  programs.bash.shellAliases = {
+    l="ls";
+    ll="ls -l";
   };
+
+  programs.git.enable = true;
+  programs.git.userName = "KianBahasadri";
+  programs.git.userEmail = "101868619+KianBahasadri@users.noreply.github.com";
 }
