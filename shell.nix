8d7ee950b83e8cede98f027794eaf4629a7691aa
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-07-06 21:11:30 -0400

    fix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 # https://nix.dev/tutorials/first-steps/declarative-shell.html
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
